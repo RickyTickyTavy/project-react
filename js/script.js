0b2a19ae7dbe95845531556756cb943dafca2929
ernets <hangoverrick92@gmail.com>
2020-08-25 10:01:38 +0300

    add movieDB script

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 "use strict";
 
-const a = false,
-			b = true;
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ?', '');
 
-console.log(2 + 2 * 2 !== 6);
+let personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+	let q1 = prompt('Один из последних просмотренных фильмов ?', ''),
+			q2 = +prompt('На сколько вы его оцените ?', '');
+			personalMovieDB.movies[q1] = q2;
+}
+
+console.log(personalMovieDB.movies);
